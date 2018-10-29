a770ff8100ca51126b40a751b76533dd0810bbd4
Monkeytoes999 <32685880+Monkeytoes999@users.noreply.github.com>
2018-10-29 17:12:11 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,21 +15,6 @@ var bot = new Discord.Client({
    autorun: true
 });
 
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true,
-});
-
-client.connect();
-
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-});
-
-
 bot.on('ready', function (evt) {
     logger.info('Connected');
     logger.info('Logged in as: ');
