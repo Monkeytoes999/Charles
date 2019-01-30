d1e51a207fdd6f75af917a5c78547ecdbd2636a0
Centurion041 <44486734+Centurion041@users.noreply.github.com>
2019-01-29 21:10:54 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -178,7 +178,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 		case 'help':
 			bot.sendMessage({
 				to: channelID,
-				message: 'To add yourself to a block, say &first or &fourth, depending on your class period. \nYou can only do this if you aren\'t in either role. If you accidently put yourself in the wrong block, contact Charles and he will change it.'
+				message: 'To add yourself to a block, say &first, &second, &third, or &fourth, depending on your class period. \nYou can only do this if you aren\'t in either role. If you accidently put yourself in the wrong block, contact Charles and he will change it.'
 			});
 			break;
          }
